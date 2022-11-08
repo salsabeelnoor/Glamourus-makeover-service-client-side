bafe2b1397d70ca493e5018eed931b419306e3ce
salsabeelnoor <salsabeelnoor@gmail.com>
2022-11-08 13:31:54 +0600

    navbar complete

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -8,9 +8,9 @@ import loc from "../../../assets/images/location.png";
 const Header = () => {
   return (
     <div className="bg-base-100">
-      <div className=" flex flex-col px-10 py-5">
-        <div className="flex flex-row lg:justify-between justify-center">
-          <div className="flex lg:justify-start justify-center lg:pl-5">
+      <div className=" flex flex-col lg:px-10 py-5">
+        <div className="lg:flex lg:flex-row lg:justify-between hidden lg:px-5">
+          <div className="flex lg:justify-start justify-center">
             <img
               className="lg:w-16 lg:h-16 w-12 h-12 mt-[-5px]"
               src={logo}
@@ -41,9 +41,36 @@ const Header = () => {
             </div>
           </div>
         </div>
-        <hr className="lg:block hidden border-1 bg-slate-400 mx-8 my-2" />
-        <div className="navbar bg-base-100">
+        <hr className="lg:block hidden border-1 bg-slate-400 mx-5 my-2" />
+        <div className="navbar bg-base-100 lg:px-5">
           <div className="navbar-start">
+            <ul className="menu menu-horizontal p-0 lg:flex hidden">
+              <li>
+                <Link className="text-lg font-medium uppercase hover:text-pink-800">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link className="text-lg font-medium uppercase hover:text-pink-800">
+                  Services
+                </Link>
+              </li>
+              <li>
+                <Link className="text-lg font-medium uppercase hover:text-pink-800">
+                  My Reviews
+                </Link>
+              </li>
+              <li>
+                <Link className="text-lg font-medium uppercase hover:text-pink-800">
+                  Add Service
+                </Link>
+              </li>
+              <li>
+                <Link className="text-lg font-medium uppercase hover:text-pink-800">
+                  Blog
+                </Link>
+              </li>
+            </ul>
             <div className="dropdown">
               <label tabIndex={0} className="btn btn-ghost lg:hidden">
                 <svg
@@ -66,70 +93,62 @@ const Header = () => {
                 className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
               >
                 <li>
-                  <Link>Home</Link>
-                </li>
-                <li tabIndex={0}>
-                  <a className="justify-between">
-                    Parent
-                    <svg
-                      className="fill-current"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                    </svg>
-                  </a>
-                  <ul className="p-2">
-                    <li>
-                      <a>Submenu 1</a>
-                    </li>
-                    <li>
-                      <a>Submenu 2</a>
-                    </li>
-                  </ul>
+                  <Link className="text-base font-medium uppercase hover:text-pink-800">
+                    Home
+                  </Link>
                 </li>
                 <li>
-                  <a>Item 3</a>
+                  <Link className="text-base font-medium uppercase hover:text-pink-800">
+                    Services
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-base font-medium uppercase hover:text-pink-800">
+                    My Reviews
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-base font-medium uppercase hover:text-pink-800">
+                    Add Service
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-base font-medium uppercase hover:text-pink-800">
+                    Blog
+                  </Link>
+                </li>
+                <li>
+                  <Link className="btn flex mr-2 bg-transparent border-2 border-pink-800 text-black hover:text-white hover:bg-pink-700 hover:border-2">
+                    Log in
+                  </Link>
+                </li>
+                <li>
+                  <Link className="btn flex mr-2 bg-transparent border-2 border-pink-800 text-black hover:text-white hover:bg-pink-700 hover:border-2 my-2">
+                    Register
+                  </Link>
                 </li>
               </ul>
             </div>
           </div>
-          <div className="navbar-center hidden lg:flex">
+          <div className="navbar-center flex lg:hidden pr-5">
+            <img className="w-12 h-12 mt-[-5px]" src={logo} alt="" />
+            <Link to="/" className="custom-font text-2xl text-[#9a063e]">
+              GlamourUS Makeover
+            </Link>
+          </div>
+          <div className="navbar-end hidden lg:flex">
             <ul className="menu menu-horizontal p-0">
               <li>
-                <a>Item 1</a>
-              </li>
-              <li tabIndex={0}>
-                <a>
-                  Parent
-                  <svg
-                    className="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                  </svg>
-                </a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
+                <Link className="btn hidden lg:flex mr-2 bg-transparent border-2 border-pink-800 text-black hover:text-white hover:bg-pink-700 hover:border-2">
+                  Log in
+                </Link>
               </li>
               <li>
-                <a>Item 3</a>
+                <Link className="btn hidden lg:flex mr-2 bg-transparent border-2 border-pink-800 text-black hover:text-white hover:bg-pink-700 hover:border-2">
+                  Register
+                </Link>
               </li>
             </ul>
-          </div>
-          <div className="navbar-end">
-            <a className="btn">Get started</a>
           </div>
         </div>
       </div>
